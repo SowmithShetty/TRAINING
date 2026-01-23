212241e159b7d51f92146bc161e9c1fa4a5fc1ab
SowmithShetty <sowmithshetty01152@gmail.com>
2026-01-23 15:41:40 +0530

    Update even_digit_sum.c

--- even_digit_sum.c
+++ even_digit_sum.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 int main(){
-    int n;
+    int n,sum=0,even=0;
     printf("Enter the number\n");
     scanf("%d",&n);
-    
+    while(n!=0)
+    {
+        even=n%10;
+        if(even%2==0){
+            sum=sum+even;
+        }n=n/10;
+    }
+    printf("Sum of even digits of a number is :%d",sum);   
 }
